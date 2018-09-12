92c70b05eda2657643340beaddb12e0fb4c7efd5
Prakhil-tp <prakhil.tp@gmail.com>
2018-09-12 12:24:12 +0530

    es6 and babel setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      click :0
+    };
+  }
+  handleClick = () => {
+    let click = this.state.click;
+    this.setState({click:click+1});
+  }
   render(){
     return(
       <div>
         <h1>react starter</h1>
+        <div>
+          <button onClick={this.handleClick}>click me</button>
+        </div>
+        <p>button clicked {this.state.click} times</p>
       </div>
     );
   }
